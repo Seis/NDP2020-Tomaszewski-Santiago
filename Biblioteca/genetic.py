1fbc47ebf1b8576c9f36caa8397c84ac04d8ca40
ichbinvi <marcos.tomaszewski@grad.ufsc.br>
2019-12-21 16:13:59 -0300

    adicionado primeiro ga\n adicionado projeto biblioteca genetic.py

---
+++ Biblioteca/genetic.py
@@ -0,0 +1,42 @@
+import random
+import datetime
+import time
+import os
+
+def _generateParent(length, geneSet): 
+    genes = []
+    while len(genes) < length:
+        sampleSize = min(length - len(genes), len(geneSet))
+        genes.extend(random.sample(geneSet, sampleSize))
+    return ''.join(genes)
+
+def _mutate (parent, geneSet):
+    index = random.randrange(0, len(parent))
+    childGenes = list(parent)
+    newGene, alternate = random.sample(geneSet,2)
+    childGenes[index] = alternate \
+        if newGene == childGenes[index] \
+        else newGene
+    return ''.join(childGenes)
+
+def getBest(getFitness, targetLen, optimalFitness, geneSet, display):
+    random.seed()
+    startTime = datetime.datetime.now()
+    bestParent = _generateParent(len(target), '')
+    bestFitness = getFitness(bestParent)
+    display(bestParent)
+    if bestFitness >= optimalFitness:
+        return bestParent
+
+    while True:
+        child = _mutate(bestParent, geneSet)
+        childFitness = getFitness(child)
+
+        if bestFitness >= childFitness:
+            continue
+        time.sleep(.05)
+        display(child)
+        if childFitness >= optimalFitness:
+            return child
+        bestFitness = childFitness
+        bestParent = child
---
+++ First GA/ga.py
@@ -0,0 +1,66 @@
+import random
+import datetime
+import time
+import os
+
+
+geneSet="1234567890qwertyuiopasdfghjklzxcvbnmçÇQWERTYUIOPASDFGHJKLZXCVBNM,.@-§áÁéÉóÓãÃõÕêÊẽẼíÍ\n "
+target="Tá fazendo mais estrago que ponto 40\nFazendo mais barulho que tiro de oitão\nE nessa boca linda, o cigarro de menta\nVoltou pra casa cedo, quase sem noção"
+
+
+
+
+def generateCleanParent(length): 
+    genes = []
+    while len(genes) < length:
+        genes.extend(' ')
+    return ''.join(genes)
+
+def generateParent(length): 
+    genes = []
+    while len(genes) < length:
+        sampleSize = min(length - len(genes), len(geneSet))
+        genes.extend(random.sample(geneSet, sampleSize))
+    return ''.join(genes)
+
+def getFitness(guess):
+    correct = 0
+    for (expected,atual) in zip(target, guess):
+        if expected == atual:
+            correct += 1
+    return correct
+
+def mutate (parent):
+    index = random.randrange(0, len(parent))
+    childGenes = list(parent)
+    newGene, alternate = random.sample(geneSet,2)
+    childGenes[index] = alternate \
+        if newGene == childGenes[index] \
+        else newGene
+    return ''.join(childGenes)
+
+def display(guess):
+    timeDiff = datetime.datetime.now() - startTime
+    fitness = getFitness(guess)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("{0}".format(guess))
+    # print("{0}\t{1}\t{2}".format(guess, fitness, str(timeDiff)))
+
+random.seed()
+startTime = datetime.datetime.now()
+bestParent = generateCleanParent(len(target))
+bestFitness = getFitness(bestParent)
+display(bestParent)
+
+while True:
+    child = mutate(bestParent)
+    childFitness = getFitness(child)
+
+    if bestFitness >= childFitness:
+        continue
+    time.sleep(.05)
+    display(child)
+    if childFitness >= len(bestParent):
+        break
+    bestFitness = childFitness
+    bestParent = child
