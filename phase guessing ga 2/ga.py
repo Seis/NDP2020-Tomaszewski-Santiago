59e6b5671c24b90ab786d10833cd5058665cf6eb
ichbinvi <marcos.tomaszewski@grad.ufsc.br>
2019-12-27 10:45:20 -0300

    pg ga2 auto teste pronto

--- phase guessing ga 2/ga.py
+++ phase guessing ga 2/ga.py
@@ -7,15 +7,6 @@ import time
 # limpar tela
 import os
 
-# numero de pais
-numeroPais = 4
-# inicializa numero de geracao
-generation = 0
-# define os caracteres possiveis
-geneSet="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'-,.@§çÇáÁéÉóÓãÃõÕêÊẽẼíÍ1234567890\n "
-# define o objetivo
-target="Last things last\nBy the grace of the fire and the flames\nYou're the face of the future\nThe blood in my veins, oh-ooh\nThe blood in my veins, oh-ooh\nBut they never did, ever lived, ebbing and flowing\nInhibited, limited\nTill it broke open and rained down\nAnd rained down, like"
-target="baseado"
 def touplesSort(e):
   return e[2]
 
@@ -40,7 +31,7 @@ def generateParent(length):
     return ''.join(genes)
 
 # retorna o numero de caracteres certos em guess comparado a target
-def getFitness(guess):
+def getFitness(guess, target):
     # caracteres corretos
     correct = 0
     # gera uma tupla com (expected, atual) para cada entrada em (target, guess) e percorre todas as tuplas
@@ -52,7 +43,7 @@ def getFitness(guess):
     return correct
 
 # cria uma mutacao de um cromossomo em parent a partir de um ponto aleatorio
-def mutate (parent):
+def mutate (parent,geneSet):
     # define aleatoriamente um ponto de mutacao dentro do parent
     index = random.randrange(0, len(parent))
     # cria um filho com os genes do parent
@@ -84,90 +75,105 @@ def display(guess):
     print("{0}\t{1}".format(guess, generation))
     # print("{0}\t{1}\t{2}".format(guess, fitness, str(timeDiff)))
 
-# inicializacao das listas
-bestParent = []
-child = []
-bestFitness = []
-childFitness = []
-absoluteBestCromossome = 0
-absoluteBestFitness = 0
 
-# inicializa o random
-random.seed()
-# define o startTime com a hora atual
-startTime = datetime.datetime.now()
-# define os primeiros pais (como um conjunto de genes do mesmo tamanho do alvo, mas todos " ")
-for x in range(numeroPais):
-    bestParent.append(generateCleanParent(len(target)))
-# armazena o fitness dos pais
-for x in bestParent:
-    bestFitness.append(getFitness(x))
+def foneoq():
 
-while True:
-    # aumenta o contador de geracoes
-    generation += 1
-    
-    # cria um filho a partir de uma mutacao de cada pai
-    for x in bestParent:
-        child.append(mutate(x))
-    # calcula e armazena o fitness do filho
-    for x in child:
-        childFitness.append(getFitness(x))
-
-    # adiciona os filhos aos pais e o fitness dos pais ao dos filhos
-    bestParent.extend(child)
-    bestFitness.extend(childFitness)
+    # numero de pais
+    numeroPais = 4
+    # inicializa numero de geracao
+    generation = 0
+    # define os caracteres possiveis
+    geneSet="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'-,.@§çÇáÁéÉóÓãÃõÕêÊẽẼíÍ1234567890\n "
+    # define o objetivo
+    target="Last things last\nBy the grace of the fire and the flames\nYou're the face of the future\nThe blood in my veins, oh-ooh\nThe blood in my veins, oh-ooh\nBut they never did, ever lived, ebbing and flowing\nInhibited, limited\nTill it broke open and rained down\nAnd rained down, like"
+    target="baseado em fatos reais"
 
 
-    bestTouples = []
-    lastParent = []
-    for thisParent in bestParent:
-        thisFitness = getFitness(thisParent)
-        if (lastParent != thisParent):
-            indice = 0
-        else:
-            indice += 1
-        if thisFitness > absoluteBestFitness:
-            absoluteBestFitness = thisFitness
-            absoluteBestCromossome = thisParent
-        bestTouples.append((thisParent, indice, thisFitness))
-        lastParent = thisParent
 
-    bestTouples.sort(key=touplesSort)
-
+    # inicializacao das listas
     bestParent = []
-    bestFitness = []
-
-    for x in range(numeroPais):
-        elemento = bestTouples.pop(-1)
-        bestParent.append(elemento[0])
-        bestFitness.append(elemento[2])
-    # time.sleep(.00005)
-    # if generation > 5:
-    #     break
-
-    # print(str(len(bestParent)))
-
-    # mostra o filho (que nesse ponto ja sabemos que e melhor que o seu pai)
-    # display(child)
-    # se o fitness do filho for igual ao tamanho do melhor pai 
-    #### (nesse caso poderia ser o tamanho do melhor pai, do filho, do target)
-    #### encerra o algoritmo, ja que a melhor solucao tem todos os caracteres corretos
-    if absoluteBestFitness >= len(target):
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print(absoluteBestCromossome + " \n" + str(generation) + " geracoes\n" + str(datetime.datetime.now() - startTime))
-        return generation
-        break
-    # caso o melhor fitness nao esteja perfeito, ele e pelo menos melhor que seu pai
-    #### portanto sera mantido para a proxima geracao
-    #### define o fitness do filho como melhor fitness
-    # bestFitness = childFitness
-    # e define o filho como melhor solucao
-    # bestParent = child
-
-
-
     child = []
+    bestFitness = []
+    childFitness = []
+    absoluteBestCromossome = 0
+    absoluteBestFitness = 0
 
-    #### rinse and repeat
-    #limpe as lista fei
+    # inicializa o random
+    random.seed()
+    # define o startTime com a hora atual
+    startTime = datetime.datetime.now()
+    # define os primeiros pais (como um conjunto de genes do mesmo tamanho do alvo, mas todos " ")
+    for x in range(numeroPais):
+        bestParent.append(generateCleanParent(len(target)))
+    # armazena o fitness dos pais
+    for x in bestParent:
+        bestFitness.append(getFitness(x,target))
+
+    while True:
+        # aumenta o contador de geracoes
+        generation += 1
+        
+        # cria um filho a partir de uma mutacao de cada pai
+        for x in bestParent:
+            child.append(mutate(x,geneSet))
+        # calcula e armazena o fitness do filho
+        for x in child:
+            childFitness.append(getFitness(x,target))
+
+        # adiciona os filhos aos pais e o fitness dos pais ao dos filhos
+        bestParent.extend(child)
+        bestFitness.extend(childFitness)
+
+
+        bestTouples = []
+        lastParent = []
+        for thisParent in bestParent:
+            thisFitness = getFitness(thisParent,target)
+            if (lastParent != thisParent):
+                indice = 0
+            else:
+                indice += 1
+            if thisFitness > absoluteBestFitness:
+                absoluteBestFitness = thisFitness
+                absoluteBestCromossome = thisParent
+            bestTouples.append((thisParent, indice, thisFitness))
+            lastParent = thisParent
+
+        bestTouples.sort(key=touplesSort)
+
+        bestParent = []
+        bestFitness = []
+
+        for x in range(numeroPais):
+            elemento = bestTouples.pop(-1)
+            bestParent.append(elemento[0])
+            bestFitness.append(elemento[2])
+        # time.sleep(.00005)
+        # if generation > 5:
+        #     break
+
+        # print(str(len(bestParent)))
+
+        # mostra o filho (que nesse ponto ja sabemos que e melhor que o seu pai)
+        # display(child)
+        # se o fitness do filho for igual ao tamanho do melhor pai 
+        #### (nesse caso poderia ser o tamanho do melhor pai, do filho, do target)
+        #### encerra o algoritmo, ja que a melhor solucao tem todos os caracteres corretos
+        if absoluteBestFitness >= len(target):
+            # os.system('cls' if os.name == 'nt' else 'clear')
+            # print(absoluteBestCromossome + " \n" + str(generation) + " geracoes\n" + str(datetime.datetime.now() - startTime))
+            return generation
+            break
+        # caso o melhor fitness nao esteja perfeito, ele e pelo menos melhor que seu pai
+        #### portanto sera mantido para a proxima geracao
+        #### define o fitness do filho como melhor fitness
+        # bestFitness = childFitness
+        # e define o filho como melhor solucao
+        # bestParent = child
+
+
+
+        child = []
+
+        #### rinse and repeat
+        #limpe as lista fei
--- phase guessing ga 2/main.py
+++ phase guessing ga 2/main.py
@@ -1,3 +1,25 @@
 import ga
 
-print(ga.main())
+results = []
+
+size = 10
+file = open("results for 10000 predefined ","w") 
+file.write("results for 10000 predefined")
+
+for x in range(1,size):
+    results.append(ga.foneoq())
+    file.write("\n" + str(x) +  " | " + str(results[-1]))
+    if not int(x % 10):
+        print(x)
+
+media = 0
+for x in results:
+    media += x
+
+file.write("\n\n\n\n\n\n")
+
+
+file.write("\nmedia  " + str(media/size))
+file.write("\nminimo " + str(int(min(results))))
+file.write("\nmaximo " + str(int(max(results))))
+file.close() 
